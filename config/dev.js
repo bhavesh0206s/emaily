16d026d6409a4c6e8af984acee87eddfaa51a2cc
Bhavesh Suthar <43042325+bhavesh0206s@users.noreply.github.com>
2020-10-09 18:41:58 +0530

    added dev.js

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: "499432353997-s8p51aaeqesfeck0pvvca65jaefohgn5.apps.googleusercontent.com",
+  googleClentSecret: "hnNfezSH_ZB4v-JG8e2f7e1J",
+  mongoURI: "mongodb+srv://bhavesh:bhavesh1234@cluster0-awgwv.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "ifdsjkgdsjidsakjkdsnfdjienjkcs",
+  stripePublishableKey: "pk_test_lpMw9oikKL8DVa6X9I9LHFUY00quGahjfm",
+  stripeSecretKey: "sk_test_t814Ha8XwkhHn1g5rQMfLfi2001HshT58f",
+  sendGridKey: "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",
+  redirectDomain: "http://locahost:3000"
+}
