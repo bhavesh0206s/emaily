8a6f0575db498d7bd98d543086a41fa45b89c8af
DziadDawid <53613664+DziadDawid@users.noreply.github.com>
2020-10-09 19:04:52 +0200

    Home page redesign

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {ReactComponent as Background1} from "../emaily_background1.svg"
+import {ReactComponent as Background2} from "../emaily_background2.svg"
 
 const Landing = () => {
   return (
-    <div style={{textAlign: 'center'}}>
+    <>
+      <div style={{position: "absolute", left: 0, zIndex: "-1"}}>
+        <Background1/>
+      </div>
+    <div style={{textAlign: 'center',fontFamily: "Poppins", color: "#2D2D2D", height: "2em"}}>
       <h1>Emily!</h1>
       <h5>
        Collect feedback from your Users
       </h5> 
     </div>
+    <div style={{position: "absolute", bottom: 0, right: 0, zIndex: "-1"}}>
+        <Background2/>
+      </div>
+    </>
   );
 }
  
